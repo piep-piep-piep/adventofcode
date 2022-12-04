3df9ce54484872918c632d83c818fb510fe5613c
Yann Vanbiervliet <yann.vanbiervliet@gmx.ch>
2022-12-04 11:20:40 +0100

    day 4, part 1

---
+++ 2022/day4/part1.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+import os
+
+class Assignment:
+    def __init__(self, start:int, end:int):
+        self.start = start
+        self.end = end
+    def check_if_fully_contained(self, a:Assignment):
+        return self.start <= a.start and self.end >= a.end
+
+def main():
+    file = open(os.path.join(os.path.dirname(__file__), 'input.txt'), 'r')
+    lines = file.readlines()
+
+    count = 0
+    for line in lines:
+        line = line.strip()
+        line = line.split(",")
+        a1 = Assignment(int(line[0].split("-")[0]), int(line[0].split("-")[1]))
+        a2 = Assignment(int(line[1].split("-")[0]), int(line[1].split("-")[1]))
+        if a1.check_if_fully_contained(a2) or a2.check_if_fully_contained(a1):
+            count += 1
+
+    print(count)
+
+if __name__=="__main__":
+    main()
